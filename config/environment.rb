e84d3aed6c22443c88511e0520d2f06b0edf8b5b
Eva <yi.zheng1709@gmail.com>
2021-02-02 12:54:37 -0500

    adds the new require_relative files to environment.rb

--- config/environment.rb
+++ config/environment.rb
@@ -1,14 +1,22 @@
-require_relative '../lib/Dragon.rb'
+
 require_relative '../lib/Elf.rb'
 require_relative '../lib/Giant.rb'
-require_relative '../lib/Goblin.rb'
 require_relative '../lib/Human.rb'
-require_relative '../lib/MythPlace.rb'
+require_relative '../lib/Jinook.rb'
 require_relative '../lib/Race.rb'
+
+require_relative '../lib/Dragon.rb'
+require_relative '../lib/Goblin.rb'
+require_relative '../lib/Imp.rb'
+require_relative '../lib/Karen.rb'
+require_relative '../lib/Santa.rb'
+
+require_relative '../lib/QuizMaster.rb'
+
+require_relative '../lib/MythPlace.rb'
 require_relative '../lib/Rubytopia.rb'
 require_relative '../lib/Scraper.rb'
-require_relative '../lib/QuizMaster.rb'
-require_relative '../lib/Karen.rb'
+
 require 'colorize'
 
 RubytopiaGame.new.start
